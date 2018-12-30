292dbcb10cd1add7f5de83e2d103ee2e941e6e11
Ascold Channe <ascold96@gmail.com>
2018-12-30 11:12:00 +0200

    remove auth module

--- src/assets/scripts/auth.js
+++ src/assets/scripts/auth.js
@@ -1,15 +1,12 @@
 import switchCards from '@modules/switchCards';
-import authorization from '@modules/authorization';
 
 document.addEventListener('DOMContentLoaded', () => {
     
     let linkAuth = document.querySelector('#to-auth'),
         flipContainer = document.querySelector('#flip-container'),
-        signInForm = document.querySelector('#sign-in'),
+        //signInForm = document.querySelector('#sign-in'),
         signInReset = document.querySelector('#log-reset');
     
     //Swipping cards 'welcome' an 'authorization'
     switchCards(linkAuth, flipContainer, signInReset);
-    //Validation in 'authorization card'
-    authorization(signInForm);
 });
--- src/assets/scripts/modules/switchCards.js
+++ src/assets/scripts/modules/switchCards.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const switchCards = (linkAuth, flipContainer, signInReset) => {
-    document.body.addEventListener('click', (e) => {
+export const switchCards = (linkAuth, flipContainer, signInReset) => {
+    document.body.addEventListener('click', function(e) {
         //Click in auth or reset or out of flip container
         if( (!e.target.closest('.flip') || e.target === signInReset) ) {
             //If click out of front flip container - no flip
